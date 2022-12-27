19813c0b9e8a806866fbb5dfcff1fc676d270fd3
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-12-27 18:30:16 +0100

    test

--- src/components/navigation/HorizontalNavBar.js
+++ src/components/navigation/HorizontalNavBar.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import {Context} from "../../Context"
+import jwt_decode from "jwt-decode";
 const HorizontalNavBar = () => {
+    const {token}=useContext(Context)
+    const [activeLink, setActiveLink] = useState('');
+    const handleClick = (path) => {
+        setActiveLink(path);
+      };
+    const user= token?jwt_decode(token):"";
     return (
         <div className="px-4 mx-auto mt-5 sm:px-6 md:px-8">
         <div className="px-4 mx-auto max-w-5xl sm:px-6 lg:px-8">
             <div className="border-b border-gray-200">
                 <nav className="flex -mb-px space-x-10">
-                    <Link href="/admin/addcategorie" to="/admin/addcategorie" className="py-4 text-sm font-medium text-indigo-600 transition-all duration-200 border-b-2 border-indigo-600 whitespace-nowrap"> Categorie</Link>
-
-                    <Link  href="/vendeur/addproduit" to="/vendeur/addproduit" className="py-4 text-sm font-medium text-gray-500 transition-all duration-200 border-b-2 border-transparent hover:border-gray-300 whitespace-nowrap"> Ajout Produit </Link>
-
-
-                    <Link href="#" className="py-4 text-sm font-medium text-gray-500 transition-all duration-200 border-b-2 border-transparent hover:border-gray-300 whitespace-nowrap"> Boutique </Link>
+                   
+                   {token?(user.role[0].authority)==="Vendeur-livreur"?(
+                     <>
+                     <Link  href="/vendeur/addproduit"  onClick={()=> handleClick('/admin/addcategorie')} to="/vendeur/addproduit" className={`py-4 text-sm font-medium ${activeLink==="/admin/addcategorie"?' text-indigo-600  border-indigo-600':' text-gray-500  border-transparent hover:border-gray-300 ' } transition-all duration-200  border-b-2  whitespace-nowrap`}  >Produit</Link>
+                     <Link  href="/vendeur/addproduit"  onClick={()=> handleClick('/vendeur/boutique')} to="/vendeur/boutique" className={`py-4 text-sm font-medium ${activeLink==="/vendeur/boutique"?' text-indigo-600  border-indigo-600':' text-gray-500  border-transparent hover:border-gray-300 ' } transition-all duration-200  border-b-2  whitespace-nowrap`}  >Boutique</Link>
+                   </>
+                   ):(
+                    <>
+                    <Link  href="/admin/addcategorie"  onClick={()=> handleClick('/admin/addcategorie')} to="/admin/addcategorie" className={`py-4 text-sm font-medium ${activeLink==="/admin/addcategorie"?' text-indigo-600  border-indigo-600':' text-gray-500  border-transparent hover:border-gray-300 ' } transition-all duration-200  border-b-2  whitespace-nowrap`}  > Categorie</Link>
+                    <Link  href="/admin/alluser"  onClick={()=> handleClick('/admin/alluser')} to="/admin/alluser" className={`py-4 text-sm font-medium ${activeLink==="/admin/alluser"?' text-indigo-600  border-indigo-600':' text-gray-500  border-transparent hover:border-gray-300 ' } transition-all duration-200  border-b-2  whitespace-nowrap`}  > All user</Link>
+                    </>
+                   ):"Vous ne verrez rien"}
+                    
+                    
                 </nav>
             </div>
         </div>
