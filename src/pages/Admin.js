c1d74944ce4a1757ceef64f6c1f1dbafb81a4488
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-12-30 01:41:25 +0100

    adding elements

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
 import AddCategorie from '../components/categorie/AddCategorie'
 import ProviderCategorie from '../components/categorie/ContextCategorie'
 const Admin = () => {
