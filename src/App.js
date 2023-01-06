e06f9dfd517b33db503b16dd12b4c96f6e61ab0a
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2023-01-06 09:37:38 +0100

    modifying home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 import Footer from "./components/navigation/Footer";
 import NavBar from "./components/navigation/NavBar";
 import Signin from "./components/authentification/Signin";
@@ -20,7 +20,6 @@ import { Context } from "./Context";
 
 
 function App() {
-  const navigate = useNavigate();
   const { getCookie, isValidToken, tokeValid } = useContext(Context);
   const [firstRender, setFirstRender] = useState(false);
   const data = getCookie();
